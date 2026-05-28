3e711917596f06d35eeb421bf67dea87a65dcf59
Siddhant Singh <siddhant24100@iiitnr.edu.in>
2026-05-28 20:17:52 +0530

    solved sort colors

---
+++ Array/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for(int i=0; i<nums.length; i++){
+            count[nums[i]]++;
+        }
+        
+        int j=0;
+        for(int i=0; i<=2; i++){
+            while(count[i] != 0){
+                nums[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
+        
+    }
+}
