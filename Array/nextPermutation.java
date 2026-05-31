bac472eb8df6bccf799cf451c22b9a973964a05f
Siddhant Singh <siddhant24100@iiitnr.edu.in>
2026-05-31 17:34:02 +0530

    Add implementation of nextPermutation algorithm

---
+++ Array/nextPermutation.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int piv = -1;
+        for(int i = n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                piv = i+1;
+                break;
+            }
+        }
+        if(piv == -1){
+            int i = 0;
+            int j = n-1;
+            while(i<=j){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++; j--;
+            }
+        }
+
+        for(int i=n-1; i>piv; i--){
+            if(nums[i]>nums[piv]){
+                int temp = nums[i];
+                nums[i] = nums[piv];
+                nums[piv] = temp;
+                break
+            }
+        }
+        int i = piv;
+        int j = n-1;
+        while(i <= j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
+        }
+    }
+}
